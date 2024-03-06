0d15a10ff4b6973498c8a72826d89b2ab5524321
Changhu Cheng <changhu.cheng@shopee.com>
2024-03-06 19:01:02 +0800

    2024-03-06 19:01:02

--- src/testMyers.js
+++ src/testMyers.js
@@ -7,113 +7,10 @@ export default function testMyers() {
   myers2(source, target);
 
   // myers1('AAA', 'AAA');
-  myers2('AAA', 'AAA');
+  // myers2('AAA', 'AAA');
 
   // myers1('A', 'AB');
-  myers2('A', 'AB');
-}
-
-function myers1(source, target) {
-  const sl = source.length;
-  const tl = target.length;
-
-  const diagonal = (i, j) => {
-    while (i < sl && j < tl && source[i + 1] === target[j + 1]) {
-      i++;
-      j++;
-    }
-    return [i, j];
-  };
-  console.log(diagonal(6, 5));
-
-  const downward = (i, j) => {
-    if (j >= tl - 1) {
-      return null;
-    }
-    return diagonal(i, j + 1);
-  };
-
-  const rightward = (i, j) => {
-    if (i >= sl - 1) {
-      return null;
-    }
-    return diagonal(i + 1, j);
-  };
-
-  let queue = [diagonal(-1, -1)];
-  let depth = 0;
-  console.log('start: ', JSON.stringify(queue), depth);
-
-  const fromMap = new Map();
-
-  while (queue.length) {
-    const _queue = [];
-    for (let [i, j] of queue) {
-      const from = `${i},${j}`;
-      const tempDownward = downward(i, j);
-      const tempRightward = rightward(i, j);
-      console.log('temp: ', tempDownward, tempRightward);
-      // if (tempDownward && tempDownward[0] === sl && tempDownward[1] === tl
-      //   || tempRightward && tempRightward[0] === sl && tempRightward[1] === tl
-      // ) {
-      //   _queue = [];
-      //   depth++;
-      //   console.log('end', tempDownward, tempRightward, depth);
-      //   break;
-      // }
-      if (_queue.length === 0) {
-        if (tempDownward) {
-          fromMap.set(tempDownward.join(','), from);
-          _queue.push(tempDownward);
-        }
-        if (tempRightward) {
-          fromMap.set(tempRightward.join(','), from);
-          _queue.push(tempRightward);
-        }
-        // tempDownward && _queue.push(tempDownward);
-        // tempRightward && _queue.push(tempRightward);
-        continue;
-      }
-
-      const lastRightward = _queue[_queue.length - 1];
-      if (!tempDownward || tempDownward[0] < lastRightward[0]) {
-        if (tempRightward) {
-          fromMap.set(tempRightward.join(','), from);
-          _queue.push(tempRightward);
-        }
-        // tempRightward && _queue.push(tempRightward);
-        continue;
-      }
-
-      _queue.pop();
-      fromMap.set(tempDownward.join(','), from);
-      _queue.push(tempDownward);
-      if (tempRightward) {
-        fromMap.set(tempRightward.join(','), from);
-        _queue.push(tempRightward);
-      }
-      // tempRightward && _queue.push(tempRightward);
-    }
-    queue = _queue;
-    if (queue.some(([_i, _j]) => _i === sl && _j === tl)) {
-      depth++;
-      break;
-    }
-    if (queue.length) {
-      console.log('next queue: ', JSON.stringify(queue));
-      depth++;
-    }
-  }
-
-  console.log(depth);
-  console.log(fromMap);
-  const paths = [`${sl},${tl}`];
-  let from = fromMap.get(paths[0]);
-  while (from) {
-    paths.unshift(from);
-    from = fromMap.get(from);
-  }
-  console.log(paths);
+  // myers2('A', 'AB');
 }
 
 function myers2(source, target) {
@@ -221,6 +118,140 @@ function myers2(source, target) {
   while (from) {
     paths.unshift(from);
     from = fromMap.get(from.coords);
+  }
+  console.log(paths);
+
+  const pathsMap = new Map(paths.map(({ coords, type }) => [coords, type]));
+  const ops = [];
+  console.log(pathsMap);
+  let i = -1;
+  let j = -1;
+  while (i < sl && j < tl) {
+    if (source[i + 1] && source[i + 1] === target[j + 1]) {
+      ops.push(source[i + 1]);
+      i++;
+      j++;
+      continue;
+    }
+    const type = pathsMap.get(`${i},${j}`);
+    switch (type) {
+      case 'delete':
+        ops.push('-' + source[i + 1]);
+        i++;
+        break;
+      case 'add':
+        ops.push('+' + target[j + 1]);
+        j++;
+        break;
+    }
+    if (i === sl - 1 && j === tl - 1) {
+      break;
+    }
+  }
+  console.log(ops);
+  console.log(source, target);
+  console.log(ops.join(' '));
+}
+
+function myers1(source, target) {
+  const sl = source.length;
+  const tl = target.length;
+
+  const diagonal = (i, j) => {
+    while (i < sl && j < tl && source[i + 1] === target[j + 1]) {
+      i++;
+      j++;
+    }
+    return [i, j];
+  };
+  console.log(diagonal(6, 5));
+
+  const downward = (i, j) => {
+    if (j >= tl - 1) {
+      return null;
+    }
+    return diagonal(i, j + 1);
+  };
+
+  const rightward = (i, j) => {
+    if (i >= sl - 1) {
+      return null;
+    }
+    return diagonal(i + 1, j);
+  };
+
+  let queue = [diagonal(-1, -1)];
+  let depth = 0;
+  console.log('start: ', JSON.stringify(queue), depth);
+
+  const fromMap = new Map();
+
+  while (queue.length) {
+    const _queue = [];
+    for (let [i, j] of queue) {
+      const from = `${i},${j}`;
+      const tempDownward = downward(i, j);
+      const tempRightward = rightward(i, j);
+      console.log('temp: ', tempDownward, tempRightward);
+      // if (tempDownward && tempDownward[0] === sl && tempDownward[1] === tl
+      //   || tempRightward && tempRightward[0] === sl && tempRightward[1] === tl
+      // ) {
+      //   _queue = [];
+      //   depth++;
+      //   console.log('end', tempDownward, tempRightward, depth);
+      //   break;
+      // }
+      if (_queue.length === 0) {
+        if (tempDownward) {
+          fromMap.set(tempDownward.join(','), from);
+          _queue.push(tempDownward);
+        }
+        if (tempRightward) {
+          fromMap.set(tempRightward.join(','), from);
+          _queue.push(tempRightward);
+        }
+        // tempDownward && _queue.push(tempDownward);
+        // tempRightward && _queue.push(tempRightward);
+        continue;
+      }
+
+      const lastRightward = _queue[_queue.length - 1];
+      if (!tempDownward || tempDownward[0] < lastRightward[0]) {
+        if (tempRightward) {
+          fromMap.set(tempRightward.join(','), from);
+          _queue.push(tempRightward);
+        }
+        // tempRightward && _queue.push(tempRightward);
+        continue;
+      }
+
+      _queue.pop();
+      fromMap.set(tempDownward.join(','), from);
+      _queue.push(tempDownward);
+      if (tempRightward) {
+        fromMap.set(tempRightward.join(','), from);
+        _queue.push(tempRightward);
+      }
+      // tempRightward && _queue.push(tempRightward);
+    }
+    queue = _queue;
+    if (queue.some(([_i, _j]) => _i === sl && _j === tl)) {
+      depth++;
+      break;
+    }
+    if (queue.length) {
+      console.log('next queue: ', JSON.stringify(queue));
+      depth++;
+    }
+  }
+
+  console.log(depth);
+  console.log(fromMap);
+  const paths = [`${sl},${tl}`];
+  let from = fromMap.get(paths[0]);
+  while (from) {
+    paths.unshift(from);
+    from = fromMap.get(from);
   }
   console.log(paths);
 }
